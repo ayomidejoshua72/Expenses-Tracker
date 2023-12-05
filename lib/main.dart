138d9f380fa07a6ca8c16a3da8e171afb7261f20
ayomidejoshua72 <ayomidejoshua72@gmail.com>
2023-12-05 07:46:48 +0100

    add app bar theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:expense_tracker/widget/expenses.dart';
 
+var kColorScheme =
+    ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 113, 61, 233));
 void main() {
   runApp(
     MaterialApp(
       theme: ThemeData().copyWith(
-        
+        useMaterial3: true,
+        colorScheme: kColorScheme,
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer,
+        ),
 
       ),
       home: const Expeneses(),

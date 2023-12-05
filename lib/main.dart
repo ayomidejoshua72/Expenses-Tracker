bd42afad488a0e556830f6ced6d6349f2a773bb2
ayomidejoshua72 <ayomidejoshua72@gmail.com>
2023-12-05 08:28:49 +0100

    allow user to determine the theme mode

--- lib/main.dart
+++ lib/main.dart
@@ -4,19 +4,29 @@ import 'package:expense_tracker/widget/expenses.dart';
 var kColorScheme =
     ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 96, 59, 181));
 
-var kDarkColorScheme =
-    ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 5, 99, 125));
+var kDarkColorScheme = ColorScheme.fromSeed(
+    brightness: Brightness.dark,
+    seedColor: const Color.fromARGB(255, 5, 99, 125));
 void main() {
   runApp(
     MaterialApp(
+      // Dark Theme
       darkTheme: ThemeData.dark().copyWith(
         useMaterial3: true,
         colorScheme: kDarkColorScheme,
         cardTheme: const CardTheme().copyWith(
-          color: kColorScheme.secondaryContainer,
+          color: kDarkColorScheme.secondaryContainer,
           margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
         ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kDarkColorScheme.primaryContainer,
+            foregroundColor: kDarkColorScheme.onPrimaryContainer,
+          ),
+        ),
       ),
+
+      // Light Theme
       theme: ThemeData().copyWith(
         useMaterial3: true,
         colorScheme: kColorScheme,

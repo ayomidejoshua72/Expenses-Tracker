23e66a27d9da20888136442f597ed116e3870b74
ayomidejoshua72 <ayomidejoshua72@gmail.com>
2023-12-06 07:28:19 +0100

    fix the display error

--- lib/widget/expenses.dart
+++ lib/widget/expenses.dart
@@ -100,7 +100,9 @@ class _ExpenesesState extends State<Expeneses> {
             )
           : Row(
               children: [
-                Chart(expenses: _registeredExpenses),
+                Expanded(
+                  child: Chart(expenses: _registeredExpenses),
+                ),
                 Expanded(
                   child: mainContent,
                 ),
